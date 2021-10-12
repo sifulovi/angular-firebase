1c9bdd947aa8bfae017a65d91e3d66cede9c04a3
ove <isiful.ovi@gmail.com>
2021-10-12 16:32:42 +0600

    fixed dashboard response

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {NotFound404Component} from './component/common/not-found404/not-found404.component';
+import {LoginComponent} from './component/login/login.component';
 
 const routes: Routes = [
+  {path: 'login', component: LoginComponent},
   {
     path: '', loadChildren: () => import('./modules/project/project.module').then(m => m.ProjectModule)
   },
--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from './service/login.service';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector   : 'app-login',
@@ -8,9 +9,25 @@ import { LoginService } from './service/login.service';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private loginService: LoginService) { }
+  validateForm!: FormGroup;
+
+  submitForm(): void {
+    for (const i in this.validateForm.controls) {
+      if (this.validateForm.controls.hasOwnProperty(i)) {
+        this.validateForm.controls[i].markAsDirty();
+        this.validateForm.controls[i].updateValueAndValidity();
+      }
+    }
+  }
+
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.validateForm = this.fb.group({
+      userName: [null, [Validators.required]],
+      password: [null, [Validators.required]],
+      remember: [true]
+    });
   }
 
 }
